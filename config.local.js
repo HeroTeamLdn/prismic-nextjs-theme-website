8e4a77dbce107bab8f6c608f6242cdce34571440
Tim Reed <tim@alloftheabove.co.uk>
2019-09-09 11:28:03 +0100

    add local config file

---
+++ config.local.js
@@ -0,0 +1,4 @@
+export const PRISMIC_API_ENDPOINT =
+  "https://teckrowebsite.cdn.prismic.io/api/v2";
+export const PRISMIC_API_TOKEN =
+  "MC5YV19RWWhFQUFDVUFGNHU3.77-9Ou-_ve-_ve-_vQgOJBPvv73vv73vv73vv73vv73vv73vv71V77-9YTtK77-977-9OFrvv70C77-977-977-9JEM";
