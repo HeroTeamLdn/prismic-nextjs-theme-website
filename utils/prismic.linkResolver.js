731ae0498ee251f2095810474ff953f7a117d3e4
Tim Reed <tim@alloftheabove.co.uk>
2019-09-12 13:30:02 +0100

    update link resolver

--- utils/prismic.linkResolver.js
+++ utils/prismic.linkResolver.js
@@ -1,9 +1,12 @@
 const linkResolver = function(doc) {
   // Pretty URLs for known types
-  if (doc.type === "blog_post") return "/hub/" + doc.uid;
+  if (doc.type === "blog_post") return "/blog/" + doc.uid;
+  if (doc.type === "basic_page") return "/basic_page/" + doc.uid;
   if (doc.type === "page") return "/page/" + doc.uid;
-  if (doc.type === "demo") return "/demo";
+  if (doc.type === "blog_landing_page") return "/blog";
+  if (doc.type === "landing_page") return "/";
   // Fallback for other types, in case new custom types get created
+  console.log(doc);
   return "/doc/" + doc.id;
 };
 
